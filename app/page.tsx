7bf0dc1cc76069c90203efd60137c0d6f02ee7e8
Aron Pachl <clefts.bagels.0c@icloud.com>
2024-01-16 16:10:14 +0100

    fix: center text

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 export default async function Page() {
   return (
     <main className="flex flex-col items-center justify-center gap-4 p-24">
-      <h1>This is the home page</h1>
+      <h1 className={'text-center'}>This is the home page</h1>
       <p className={'text-xs'}>
         This page is not secured! Login to access{' '}
         <Link className={'text-blue-400 underline'} href={'/dashboard'}>
